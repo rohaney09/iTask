2d0d37f69ee7922e81cbb31047b7943c185f71bf
Rohan Paul <136823177+rohaney09@users.noreply.github.com>
2025-07-28 18:25:26 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,11 @@ const Navbar = () => {
             <div className="container mx-auto flex justify-between items-center">
             <h1 className="text-white text-3xl font-bold">iTask</h1>
             <ul className="flex gap-12">
+              
                 <li><a href="#" className="text-white hover:text-gray-500 font-bold transition-all">Home</a></li>
                 <li><a href="#" className="text-white hover:text-gray-500 font-bold">About</a></li>
                 <li><a href="#" className="text-white hover:text-gray-500 font-bold">Tasks</a></li>
+              
             </ul>
             </div>
         </nav>
